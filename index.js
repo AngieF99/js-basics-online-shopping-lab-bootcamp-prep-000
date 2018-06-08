9fd6c1c0f1db26c2e870b141e238a0c3fe253b86
Angelica Fields <angelicafields08@gmail.com>
2018-06-08 20:37:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,9 +60,7 @@ function removeFromCart(itemName) {
        return cart
    }
  }
- else{
-     return `That item is not in your cart.`
-   }
+  return `That item is not in your cart.`
 }
 
 function placeOrder(cardNumber) {
